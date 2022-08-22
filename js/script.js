4fb5368dfb19f00ac48fde19ecc83563c6bd98f7
somesh <someshlakhe.official@gmail.com>
2022-08-22 20:11:46 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("toggler-button").addEventListener('click', function(){
+
+    document.querySelector(".bar1").classList.toggle("active");
+   document.querySelector(".bar3").classList.toggle("active");
+   document.querySelector(".bar2").classList.toggle("active");
+});
+
+
+
+
+
